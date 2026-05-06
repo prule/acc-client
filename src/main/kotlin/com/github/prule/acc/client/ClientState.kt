87a33cfb059bc97e5debc54faa1fdf7bdf2c2914
paulrule <paulrule1@gmail.com>
2026-05-06 20:57:22 +1000

    cli client sample - clean up

--- src/main/kotlin/com/github/prule/acc/client/ClientState.kt
+++
@@ -1,5 +0,0 @@
-package com.github.prule.acc.client
-
-/** Backwards-compatible alias. Prefer [ClientContext] in new code. */
-@Deprecated("Use ClientContext", ReplaceWith("ClientContext"))
-typealias ClientState = ClientContext
